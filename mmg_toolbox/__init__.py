2d2f923d65864290142bc907b564933fd8553358
Dan Porter <dan.porter@diamond.ac.uk>
2025-03-11 08:39:30 +0000

    updated initial directory

--- mmg_toolbox/__init__.py
+++ mmg_toolbox/__init__.py
@@ -10,6 +10,7 @@ __all__ = ['start_gui']
 
 
 def start_gui():
+    from .env_functions import get_data_directory
     from .tkguis import create_file_browser
-    create_file_browser()
+    create_file_browser(initial_directory=get_data_directory())
 
