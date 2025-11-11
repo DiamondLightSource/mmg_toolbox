313f33308dc5b8b5b99f8e8bb8d99536685947b2
Dan Porter <dan.porter@diamond.ac.uk>
2025-11-11 08:39:51 +0000

    add NexusPlotAndImage

--- start_dataviewer.py
+++ start_dataviewer.py
@@ -6,4 +6,9 @@ from mmg_toolbox import start_gui
 
 # start_gui('/dls/science/groups/das/ExampleData/i16/azimuths/1108750.nxs')
 # start_gui('/dls/science/groups/das/ExampleData/i16/azimuths')
-start_gui()
+# start_gui()
+
+from mmg_toolbox.tkguis.apps.nexus import create_nexus_plot_and_image
+
+# create_nexus_plot_and_image('/dls/science/groups/das/ExampleData/i16/azimuths/1108750.nxs')
+create_nexus_plot_and_image('/dls/i16/data/2025/nt43883-1/1113041.nxs')
